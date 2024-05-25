856b3fcd064315bc37c00f863b78aff065632c9c
Valentave <valentintaverna99@gmail.com>
2024-05-25 00:04:12 -0300

    Apply cache for images

---
+++ next.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/assets/(.*)', // Archivos en /public/assets
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable',
+            },
+          ],
+        },
+        {
+          source: '/src/(.*)', // Archivos en /src
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable',
+            },
+          ],
+        },
+        {
+          source: '/(.*)', // Otros archivos en /public
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable',
+            },
+          ],
+        },
+      ];
+    },
+  };
