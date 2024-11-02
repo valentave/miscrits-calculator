8bb54ffd351a0c1fdeba017cb4ae4e711c6f30e9
Valentave <valentintaverna99@gmail.com>
2024-11-01 22:25:10 -0300

    Add Patchkin and Skelepup

--- src/components/HelpWindow.jsx
+++ src/components/HelpWindow.jsx
@@ -57,6 +57,13 @@ export default function HelpWindow({setHelpActive}) {
                 {changelog &&
                 <div className="changelog-container">
                     <div className="changelog-version">
+                        <h2>v0.6.3 - November 1, 2024</h2>
+                        <hr />
+                        <ul>
+                            <li>Added Patchkin and Skelepup</li>
+                        </ul>
+                    </div>
+                    <div className="changelog-version">
                         <h2>v0.6.2 - October 23, 2024</h2>
                         <hr />
                         <ul>
